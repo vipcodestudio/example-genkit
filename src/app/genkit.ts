cb3deb627065cdb740a8d0e67f50eb8e6c92834d
vipcodestudio <avipsyaifulloh.business@gmail.com>
2024-10-13 22:05:16 +0700

    feat: setup genkit

---
+++ src/app/genkit.ts
@@ -0,0 +1,47 @@
+'use server';
+
+import * as z from 'zod';
+
+// Import the Genkit core libraries and plugins.
+import { generate } from '@genkit-ai/ai';
+import { configureGenkit } from '@genkit-ai/core';
+import { defineFlow, runFlow } from '@genkit-ai/flow';
+import { googleAI } from '@genkit-ai/googleai';
+
+// Import models from the Google AI plugin. The Google AI API provides access to
+// several generative models. Here, we import Gemini 1.5 Flash.
+import { gemini15Flash } from '@genkit-ai/googleai';
+
+configureGenkit({
+  plugins: [
+    googleAI({
+      apiKey: process.env.GOOGLE_GENAI_API_KEY,
+    }),
+  ],
+  logLevel: 'debug',
+  enableTracingAndMetrics: true,
+});
+
+const menuSuggestionFlow = defineFlow(
+  {
+    name: 'menuSuggestionFlow',
+    inputSchema: z.string(),
+    outputSchema: z.string(),
+  },
+  async (subject) => {
+    // Construct a request and send it to the model API.
+    const llmResponse = await generate({
+      prompt: `Suggest an item for the menu of a ${subject} themed restaurant`,
+      model: gemini15Flash,
+      config: {
+        temperature: 1,
+      },
+    });
+    return llmResponse.text();
+  },
+);
+
+export async function callMenuSuggestionFlow(theme: string) {
+  const flowResponse = await runFlow(menuSuggestionFlow, theme);
+  return flowResponse;
+}
